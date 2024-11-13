35039155711c3303a21b39c6ddbb5e6ddcb2033d
HuangWD <huangwelldone01@gmail.com>
2024-11-13 14:28:17 +0800

    3rd change buttons value

--- main.py
+++ main.py
@@ -2,19 +2,13 @@ import time
 
 import pyautogui as p
 
-# 图片保存路径
-paths = {
-    'reply': 'C:\\Users\\HuangWellDone\\Desktop\\PythonProject\\AutoReplyBili\\1_reply.png',
-    'ai': 'C:\\Users\\HuangWellDone\\Desktop\\PythonProject\\AutoReplyBili\\2_AI.png',
-    'insert': 'C:\\Users\\HuangWellDone\\Desktop\\PythonProject\\AutoReplyBili\\3_Insert.png',
-    'post': 'C:\\Users\\HuangWellDone\\Desktop\\PythonProject\\AutoReplyBili\\4_post.png'
-}
+buttons = {"reply": '1_reply.png', "AI": '2_AI.png', "insert": '3_insert.png', "post": '4_post.png'}
 
 
 # 点击按钮的通用函数
 def click_button(button_name, confidence=0.9, wait_time=1):
     try:
-        location = p.locateCenterOnScreen(paths[button_name], confidence=confidence)
+        location = p.locateCenterOnScreen(buttons[button_name], confidence=confidence)
         if location is not None:
             p.click(location.x, location.y)
             print(f"已点击{button_name}按钮")
@@ -26,7 +20,7 @@ def click_button(button_name, confidence=0.9, wait_time=1):
 if __name__ == '__main__':
     while True:
         click_button('reply')
-        click_button('ai', wait_time=3)
+        click_button('AI', wait_time=3)
         click_button('insert', confidence=0.7)
         click_button('post', confidence=0.7, )
         p.click(10, 300)
